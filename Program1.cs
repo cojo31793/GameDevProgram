d2c287f8fe7eaeb492f0d6ccad7d1cc3ca6d79c6
Karl R. Henderson <karlrhen@umich.edu>
2017-01-24 01:31:01 -0800

    Fixed your issue with calculateWACC
    You forgot to add a * in your math - refer to my comment.

--- Program1.cs
+++ Program1.cs
@@ -11,10 +11,10 @@ namespace Application
 
 			double hold;
 			double growthRate;
-			double debttov;
+			double debttov; 
 			double eqtoval;
 			double calculateWACC;
-			string showIntro;string a;string b;
+			string showIntro;string a;string b; // all of these should be on one line and rename a and b to be more descriptive
 
 
 			eqtoval = 0.55;
@@ -31,18 +31,22 @@ namespace Application
 
 			System.Console.Write(Environment.NewLine + "Enter the Growth Rate ");
 			hold = System.Console.Read();
-			/*System.Console.Write (Environment.NewLine + "Enter the Debt to Value Ratio");
-			debttov = System.Console.Read ();
-			if (hold < 4)
-			{growthRate = hold/100;
-				System.Console.Write (Environment.NewLine + "The company's Growth Rate is " + growthRate);
-					
+			
+			System.Console.Write (Environment.NewLine + "Enter the Debt to Value Ratio");
+			debttov = System.Console.Read();
+			if (hold < 4) // notice how i made this look a little cleaner for you
+			{
+				growthRate = hold/100;
+				System.Console.Write (Environment.NewLine + "The company's Growth Rate is " + growthRate);		
 			}
-			else{ System.Console.Write (Environment.NewLine + "Error Invalid input");
-				
+			else
+			{ 
+				System.Console.Write (Environment.NewLine + "Error Invalid input");
 			}
 
-			calculateWACC = debttov*(.06 (1 - 0.35) + 0.55 * 0.11);    */
+			calculateWACC = debttov * (.06 * (1 - 0.35) + (0.55 * 0.11));    
+			// calculateWACC = debttov * (.06 (1 - 0.35) + (0.55 * 0.11));  
+			// this was the original line...notice you forgot the extra * after the 0.06 ...this is important  
 
 		}
 	} 
