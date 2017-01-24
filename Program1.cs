544ca8c537ca74a295823349ef944418aeb99668
Cornelius Johnson <corneliusjohnson904@gmail.com>
2017-01-24 00:58:12 -0800

    Add files via upload

--- Program1.cs
+++ Program1.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace Application
 {
@@ -6,28 +6,44 @@ namespace Application
 	{
 		public static void Main (string[] args)
 		{
+		
+
+
+			double hold;
+			double growthRate;
+			double debttov;
+			double eqtoval;
+			double calculateWACC;
 			string showIntro;string a;string b;
+
+
+			eqtoval = 0.55;
 			a = "Welcome to the CloudTools Valuation Tool";b = "Copyright Wolverines & Co. All Rights Resevered.";
 
 			showIntro = a + "\n" + b;
 			System.Console.Write (showIntro);
-			double hold;
-			double growthRate;
-			double calculateWACC;
+		
+
+
+
+
 
 
 			System.Console.Write(Environment.NewLine + "Enter the Growth Rate ");
 			hold = System.Console.Read();
+			System.Console.Write (Environment.NewLine + "Enter the Debt to Value Ratio");
+			debttov = System.Console.Read ();
 			if (hold < 4)
 			{growthRate = hold/100;
 				System.Console.Write (Environment.NewLine + "The company's Growth Rate is " + growthRate);
 					
 			}
 			else{ System.Console.Write (Environment.NewLine + "Error Invalid input");
-					
 				
-
 			}
+
+			calculateWACC = debttov*(.06 (1 - 0.35) + 0.55 * 0.11);
+
 
 		}
 	} 
